2488995a692487543448ba1c3c36f5f417594cd3
poilbois-marco <marco.poilbois@edu.univ-fcomte.fr>
2023-11-10 11:51:10 +0100

    animation feu d'artifice

--- script.js
+++ script.js
@@ -5,4 +5,33 @@
     function incrementCounter() {
       clickCounter++;
       document.getElementById('counter').innerText = 'Clics : ' + clickCounter;
+      if (clickCounter === 100) {
+        launchFireworks();
+      }
     }
+
+    function launchFireworks() {
+      document.getElementById('fireworks').style.display = 'block';
+
+      // Utilisation d'Anime.js pour créer une animation de feu d'artifice
+      anime({
+        targets: '#fireworks',
+        translateY: [
+          { value: -500, duration: 1000 },
+          { value: 0, duration: 800 }
+        ],
+        opacity: [
+          { value: 0, duration: 1000 },
+          { value: 1, duration: 800 }
+        ],
+        easing: 'easeInOutQuad',
+        complete: function(anim) {
+          // Réinitialiser le compteur et masquer le feu d'artifice après l'animation
+          clickCounter = 0;
+          document.getElementById('counter').innerText = 'Clics : ' + clickCounter;
+          document.getElementById('fireworks').style.display = 'none';
+        }
+      });
+    }
+    
+
