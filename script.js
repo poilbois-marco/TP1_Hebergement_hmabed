e1d6c6d76ad6f4fca8ca8e06e0e454b13f8aa8d0
poilbois-marco <marco.poilbois@edu.univ-fcomte.fr>
2023-11-10 11:00:00 +0100

    test 2 copilot html + JS

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 
-// Get the button element from the HTML document
+let count = 0;
+
+function handleClick() {
+  count++;
+  console.log(`Button clicked ${count} times`);
+}
+
 const button = document.querySelector('button');
-
-// Initialize the click count to 0
-let clickCount = 0;
-
-// Add a click event listener to the button
-button.addEventListener('click', () => {
-  // Increment the click count
-  clickCount++;
-
-  // Log the click count to the console
-  console.log(`Button clicked ${clickCount} times`);
-
-  // Save the click count to local storage
-  localStorage.setItem('clickCount', clickCount);
-});
-
-// Retrieve the click count from local storage on page load
-clickCount = parseInt(localStorage.getItem('clickCount')) || 0;
-
-// Log the initial click count to the console
-console.log(`Button clicked ${clickCount} times`);
+button.addEventListener('click', handleClick);
